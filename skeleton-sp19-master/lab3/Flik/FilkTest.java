1a79e35464fcbd83344595e0a504c03c969c8c99
jason <ridijason@163.com>
2020-06-22 16:46:46 +0800

    lab3 is finished

---
+++ skeleton-sp19-master/lab3/Flik/FilkTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FilkTest {
+    @Test
+    public void testIsSameNumber() {
+        int a = 128;
+        int b = 128;
+        int c = 500;
+        boolean actual1 = Flik.isSameNumber(a, b);
+        boolean actual2 = Flik.isSameNumber(a, c);
+
+        assertTrue(actual1);
+        assertFalse(actual2);
+
+    }
+}
--- skeleton-sp19-master/lab3/IntList/IntListTest.java
+++ skeleton-sp19-master/lab3/IntList/IntListTest.java
@@ -69,5 +69,14 @@ public class IntListTest {
     /** If you're running this from the command line, you'll need
       * to add a main method. See ArithmeticTest.java for an
       * example. */
+    @Test
+    public void testReverse(){
+        IntList A = IntList.of(1,2,3);
+        IntList exp = IntList.of(3,2,1);
+        assertEquals(exp,IntList.reverse(A));
+        assertNotEquals(A,IntList.of(1,2,3,4,5));
+        assertEquals(null,IntList.reverse(null));
+
+    }
 
 }
