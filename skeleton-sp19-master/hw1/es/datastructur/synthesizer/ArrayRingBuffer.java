813b538199f7b404b985551fc74581f7142577b5
jason <ridijason@163.com>
2020-07-22 16:54:54 +0800

    hw1 is finished

--- skeleton-sp19-master/hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ skeleton-sp19-master/hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -1,11 +1,12 @@
 package es.datastructur.synthesizer;
+import java.lang.reflect.Array;
 import java.util.Iterator;
 
 //TODO: Make sure to that this class and all of its methods are public
 //TODO: Make sure to add the override tag for all overridden methods
 //TODO: Make sure to make this class implement BoundedQueue<T>
 
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> implements BoundedQueue<T>{
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
@@ -18,9 +19,24 @@ public class ArrayRingBuffer<T>  {
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
+
+
+    public int capacity(){
+        return rb.length;
+    }
+
+    public int fillCount(){
+        return fillCount;
+    }
+
+
     public ArrayRingBuffer(int capacity) {
         // TODO: Create new array with capacity elements.
         //       first, last, and fillCount should all be set to 0.
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
     }
 
     /**
@@ -30,7 +46,12 @@ public class ArrayRingBuffer<T>  {
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update
         //       last.
-        return;
+        if(isFull()){
+            throw new RuntimeException("Ring Buffer overflow");
+        }
+        rb[last] = x;
+        last = (last + 1) % capacity();
+        fillCount += 1;
     }
 
     /**
@@ -40,7 +61,13 @@ public class ArrayRingBuffer<T>  {
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and
         //       update first.
-        return null;
+        if (isEmpty()){
+            throw  new RuntimeException("Ring Buffer underflow");
+        }
+        T deOne = rb[first];
+        first = (first + 1) % capacity();
+        fillCount -= 1;
+        return deOne;
     }
 
     /**
@@ -50,10 +77,68 @@ public class ArrayRingBuffer<T>  {
     public T peek() {
         // TODO: Return the first item. None of your instance variables should
         //       change.
-        return null;
+        if(isEmpty()){
+            return null;
+        }
+        return rb[first];
+    }
+    public Iterator<T> iterator(){
+        return new ArrayBufferIterator();
+    }
+
+    private class ArrayBufferIterator implements Iterator<T>{
+        private int curIndex;
+        private int count;
+
+        public ArrayBufferIterator(){
+            curIndex = first;
+            count = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return count < fillCount();
+        }
+
+        @Override
+        public T next(){
+            T returnItem = rb[curIndex];
+            curIndex = (curIndex + 1) % capacity();
+            count += 1;
+            return returnItem;
+        }
+    }
+
+
+    @Override
+    public boolean equals(Object o){
+        if(this.getClass() != o.getClass()){
+            return false;
+        }
+
+        ArrayRingBuffer ob = (ArrayRingBuffer) o;
+
+        Iterator<T> thisIterator = this.iterator();
+        Iterator<T> obIterator = ob.iterator();
+
+
+        if(ob.capacity() != this.capacity()){
+            return false;
+        }else if(ob.fillCount() != this.fillCount()){
+            return false;
+        }else{
+            while(thisIterator.hasNext() && obIterator.hasNext()){
+                if(thisIterator.next() != obIterator.next()){
+                    return false;
+                }
+            }
+            }
+        return true;
+
+
     }
 
     // TODO: When you get to part 4, implement the needed code to support
     //       iteration and equals.
-}
     // TODO: Remove all comments that say TODO when you're done.
+}
--- skeleton-sp19-master/hw1/es/datastructur/synthesizer/GuitarString.java
+++ skeleton-sp19-master/hw1/es/datastructur/synthesizer/GuitarString.java
@@ -16,6 +16,10 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your buffer should be initially filled with zeros.
+        buffer = new ArrayRingBuffer<Double>((int)Math.round(SR / frequency));
+        for(int i = 0;i < buffer.capacity();i++){
+            buffer.enqueue(0.0);
+        }
     }
 
 
@@ -24,6 +28,11 @@ public class GuitarString {
         // TODO: Dequeue everything in buffer, and replace with random numbers
         //       between -0.5 and 0.5. You can get such a number by using:
         //       double r = Math.random() - 0.5;
+        for(int i = 0;i < buffer.capacity();i++){
+            buffer.dequeue();
+            double r = Math.random() - 0.5;
+            buffer.enqueue(r);
+        }
         //
         //       Make sure that your random numbers are different from each
         //       other.
@@ -36,12 +45,15 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
+        double deOne = buffer.dequeue();
+        double enOne = deOne * buffer.peek() * DECAY * 0.5;
+        buffer.enqueue(enOne);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        return buffer.peek();
     }
 }
     // TODO: Remove all comments that say TODO when you're done.
