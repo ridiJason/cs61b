87164e88f7ffa2a9d20b635897c05d20700d118c
jason <ridijason@163.com>
2020-08-04 10:51:13 +0800

    lab8 is finished

--- skeleton-sp19-master/lab8/MyHashMap.java
+++ skeleton-sp19-master/lab8/MyHashMap.java
@@ -1,4 +1,5 @@
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -7,9 +8,9 @@ public class MyHashMap<K,V> implements Map61B<K,V>{
     private static final int initialSize = 16;
     private static final double loadFactor = 0.75;
 
-    private int aSizeThresh; // array size
+    private int pSizeThresh; // array size
     private int pSize; // number of K-V pairs
-    private Bucket<K, V>[] bucket;
+    private Bucket<K, V>[] buckets;
 
     private class Bucket<K,V>{
         private K key;
@@ -60,45 +61,140 @@ public class MyHashMap<K,V> implements Map61B<K,V>{
 
 
     public MyHashMap(){
-        bucket = new Bucket[initialSize];
+        buckets = new Bucket[initialSize];
         pSize = 0;
+        pSizeThresh = (int)loadFactor * initialSize;
     }
 
     public MyHashMap(int initialSize){
-        this.aSizeThresh = initialSize;
+        buckets = new Bucket[initialSize];
+        pSize = 0;
+        pSizeThresh = (int)loadFactor * initialSize;
+    }
+
+    public MyHashMap(int initialSize, double loadFactor){
+        buckets = new Bucket[initialSize];
+        pSize = 0;
+        pSizeThresh = (int)loadFactor * initialSize;
     }
 
 
 
     @Override
     public void clear() {
-
+        buckets = new Bucket[initialSize];
+        pSize = 0;
     }
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+
+    private int hash(K key, int length) {
+        return (key.hashCode() & 0x7fffffff) % length;
     }
 
     @Override
     public V get(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to get() is null");
+        }
+        int hash = hash(key,buckets.length);
+        Bucket<K,V> bucket = get(key,hash);
+       return bucket.getValue();
+    }
+
+    public Bucket<K,V> get(K key,int hashcode){
+        Bucket bucket = buckets[hashcode];
+        while(bucket != null){
+            if(bucket.getHashcode() == hashcode && bucket.getKey() == key){
+                return bucket;
+            }
+            bucket = bucket.next;
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return 0;
+        return pSize;
     }
 
     @Override
     public void put(K key, V value) {
-
+        if (key == null) {
+            throw new IllegalArgumentException("first argument to put() is null");
+        }
+        if (value == null) {
+            remove(key);
+            return;
+        }
+        int hash = hash(key,buckets.length);
+        Bucket<K,V> bucket = buckets[hash];
+        while (bucket != null){
+            if(bucket.getHashcode() == hash && bucket.getKey() == key){
+                throw new IllegalArgumentException("the key exists");
+            }
+            bucket = bucket.next;
+        }
+        put(key,value,hash);
     }
+
+
+    public void put(K key, V value, int hashcode){
+        Bucket bucket = new Bucket(key,value,buckets[hashcode],hashcode); //为找到此结点只能插入数组后的第一位
+        buckets[hashcode] = bucket;
+        pSize += 1;
+        if(pSize > pSizeThresh){
+            resize();
+        }
+        return;
+    }
+
+    public void resize(){
+        int newArraySize = buckets.length * 2;
+        Bucket<K,V>[] newbuckets = new Bucket[newArraySize];
+        for (int i = 0; i < buckets.length; i++){
+            Bucket<K,V> bucket = buckets[i];
+            while (bucket != null){
+                Bucket<K,V> oldnext = bucket.next;
+                int newhash = hash(bucket.getKey(),newArraySize);
+                bucket.setHashcode(newhash);
+                bucket.setNext(newbuckets[newhash]);
+                newbuckets[newhash] = bucket;
+                bucket = oldnext;
+            }
+        }
+        buckets = newbuckets;
+        pSizeThresh = (int)loadFactor * newbuckets.length;
+    }
+
 
     @Override
     public Set<K> keySet() {
-        return null;
+        Set<K> set = new HashSet<>();
+        for(int i = 0; i < buckets.length; i++){
+            Bucket<K,V> bucket = buckets[i];
+            while (bucket != null){
+                set.add(bucket.key);
+                bucket = bucket.next;
+            }
+        }
+        return set;
     }
+
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
+
+
 
     @Override
     public V remove(K key) {
@@ -107,11 +203,6 @@ public class MyHashMap<K,V> implements Map61B<K,V>{
 
     @Override
     public V remove(K key, V value) {
-        return null;
-    }
-
-    @Override
-    public Iterator<K> iterator() {
         return null;
     }
 }
